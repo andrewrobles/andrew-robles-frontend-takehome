2bcde81a1f1f5418dc87135fd13755e2ab1f22c1
Andrew Robles <me@andrewrobles.com>
2021-05-13 04:58:50 -0700

    Add question cycling

---
+++ data.js
@@ -0,0 +1,88 @@
+const quizQuestionData = [{
+    question: 'Which of the following most closely describes the language that computers innately understand?',
+    id: 'q1',
+    options: [{
+      name: 'Assembly',
+      id: 'a1'
+    }, {
+      name: 'Binary x',
+      id: 'a2'
+    }, {
+      name: 'C++',
+      id: 'a3'
+    }, {
+      name: 'Compiled Language',
+      id: 'a4'
+    }, {
+      name: 'Interpreted Language',
+      id: 'a5'
+    }]
+  },
+  {
+    question: 'What is the key difference between a script and a program?',
+    id: 'q2',
+    options: [{
+      name: 'Scripts are written in English for human developers to understand the code. Programs are the actual code.',
+      id: 'b1'
+    }, {
+      name: 'A program uses an interpreter to interpret the code into binary. A script is the interpreted binary code.',
+      id: 'b2'
+    }, {
+      name: 'Scripts talk to applications to tell them what to do. Programs talk to the computer or operating system after being translated.',
+      id: 'b3'
+    }, {
+      name: 'A script is like Microsoft Word. A program is like Adobe Photoshop.',
+      id: 'b4'
+    }]
+  },
+  {
+    question: 'Is C# a compiled language or an interpreted language?',
+    id: 'q3',
+    options: [{
+      name: 'C# is compiled to 64-bit WASM Assembly. It is a compiled language.',
+      id: 'c1'
+    }, {
+      name: 'C# is interpreted by .NET and Unity. It is an interpreted language.',
+      id: 'c2'
+    }, {
+      name: 'C# is compiled to Intermediary Language (IL) by the JIT. It has traits of both compiled and interpreted languages.',
+      id: 'c3'
+    }, ]
+  },
+  {
+    question: 'Which of these is not true about C#?',
+    id: 'q4',
+    options: [{
+      name: 'Code written once can often be used on multiple platforms.',
+      id: 'd1'
+    }, {
+      name: 'It is faster than old programming languages, like C.',
+      id: 'd2'
+    }, {
+      name: 'It handles memory management for you.',
+      id: 'd3'
+    }, {
+      name: 'It uses a JIT compiler so it can be portable, like Java.',
+      id: 'd4'
+    }]
+  },
+  {
+    question: 'The characters "", {}, () are examples of:',
+    id: 'q5',
+    options: [{
+      name: 'Reserved Keywords',
+      id: 'e1'
+    }, {
+      name: 'Symbols',
+      id: 'e2'
+    }, {
+      name: 'Bracket Pairs',
+      id: 'e3'
+    }, {
+      name: 'Dereference Operators',
+      id: 'e4'
+    }]
+  }
+]
+
+export default quizQuestionData
