1b7bb42df841b7e7271dfaa3701b7cf40707ca01
Andrew Robles <me@andrewrobles.com>
2021-05-14 05:41:17 -0700

    Desktop navbar size

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import styles from './Navbar.module.css'
 import Image from 'next/image'
 
 export default function Navbar() {
-    return <nav className={`shadow navbar navbar-dark bg-primary p-0`}>
+    return <nav className={`shadow navbar navbar-dark bg-primary p-0 ${styles.navbarCustom}`}>
         <div className={`${styles.logo} d-flex justify-content-center py-2`}>
-            <Image src="/MasteryCodingLogo.png" width='104px' height='34'/>
+            <Image src="/MasteryCodingLogo.png" width='104px' height='37'/>
         </div>
     </nav>
 }
