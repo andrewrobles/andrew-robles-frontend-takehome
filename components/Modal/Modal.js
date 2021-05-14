f02b74bbcf506648837a6f5784a245be7a62a408
Andrew Robles <me@andrewrobles.com>
2021-05-14 12:06:34 -0700

    grade wip

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -4,10 +4,11 @@ import Modal from 'react-bootstrap/Modal'
 import Button from '../Button/Button'
 
 import styles from './Modal.module.css'
+import quizQuestionData from '../../quizQuestionData'
 
 export default function Results() {
     const answerKey=['a1', 'b2', 'c3', 'd4', 'e4']
-    const studentAnswers=['a1', '', 'c3', '', '', '']
+    const studentAnswers=['a1', -1, 'c3', -1, -1, -1]
 
     const [show, setShow] = useState(false)
 
@@ -53,4 +54,41 @@ export default function Results() {
         </Modal>
       </>
     );
+}
+
+const getResults = (answerKey, studentAnswers) => {
+  const results = [] 
+
+  for (var i = 0; i < answerKey.length; i++) {
+   const studentAnswerId = studentAnswers[i]
+   const actualAnswerId = answerKey[i]
+
+   const currResult = {
+    question: getQuestion(i, quizQuestionData),
+    studentAnswer:getAnswer(i, studentAnswerId, quizQuestionData),
+    actualAnswer:getAnswer(i, actualAnswerId, quizQuestionData),
+    isCorrect:answerKey[i]===studentAnswers[i]
+   }
+
+   results.push(currResult)
+  }
+
+  return results
+}
+
+const getQuestion = (questionIndex, quizQuestionData) => {
+  return quizQuestionData[questionIndex].question
+}
+
+const getAnswer = (questionIndex, questionId, quizQuestionData) => {
+  const currQuestion = quizQuestionData[questionIndex]
+
+  details.question = currQuestion.question
+
+  if (questionId != -1) {
+    const optionIndex = parseInt(questionId[1], 10) - 1
+    return currQuestion.options[optionIndex].name
+  } else {
+    return ''
+  }
 }
