9d6d31fa8c16a066dc08bcad0c3d5b51f52b71d1
Andrew Robles <me@andrewrobles.com>
2021-05-14 08:07:18 -0700

    Remove border from button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,7 +1,7 @@
 import styles from './Button.module.css'
 
 export default function Button(props) {
-  return <button className={`btn btn-primary ${styles.button}`} onClick={props.onClick}>
+  return <button className={`btn btn-primary border-0 ${styles.button}`} onClick={props.onClick}>
         {props.buttonText}
   </button>
 }
