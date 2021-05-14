5e80448f1fc32e5a4e6ab24a314b12ed1a1bbfea
Andrew Robles <me@andrewrobles.com>
2021-05-14 09:52:15 -0700

    Quiz results modal styling

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -1,36 +1,33 @@
 import {useState} from 'react';
 
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import Button from '../Button/Button'
 
-import styles from './Modal.module.css';
+import styles from './Modal.module.css'
 
 export default function Results() {
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false)
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
   
     return (
       <>
-        <button type="button" className={`btn ${styles.btnCustom}`} onClick={handleShow}>Sign up</button>
+        <Button onClick={handleShow} buttonText={"Start over"}/>
 
         <Modal show={show} onHide={handleClose}>
 
-          <Modal.Header closeButton>
-            <Modal.Title><i>EveryLine</i> is temporarily down</Modal.Title>
+          <Modal.Header>
+            <Modal.Title>Quiz results</Modal.Title>
           </Modal.Header>
 
           <Modal.Body>
-            <Form.Label>Sorry! We will send you an email when it's up and running</Form.Label>
-            <Form.Control className="mb-2" type="email" placeholder="Email address" />
+              Here are your quiz results:
           </Modal.Body>
         
           <Modal.Footer>
-            <Button variant="primary" onClick={handleClose}>
-              Submit
-            </Button>
+            <Button onClick={handleClose} buttonText={'Close'}/>
           </Modal.Footer>
 
         </Modal>
