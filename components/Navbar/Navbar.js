36317fe437fc670ffd95c6eb4a3cdbc5d870b43e
Andrew Robles <me@andrewrobles.com>
2021-05-14 05:21:07 -0700

    Add navbar shadow

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import styles from './Navbar.module.css'
 import Image from 'next/image'
 
 export default function Navbar() {
-    return <nav className={`navbar navbar-dark bg-primary p-0`}>
-        <span className={`${styles.logo} d-flex justify-content-center py-2`}>
+    return <nav className={`shadow navbar navbar-dark bg-primary p-0`}>
+        <div className={`${styles.logo} d-flex justify-content-center py-2`}>
             <Image src="/MasteryCodingLogo.png" width='104px' height='34'/>
-        </span>
+        </div>
     </nav>
 }
